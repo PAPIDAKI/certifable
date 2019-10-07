40748fdb015c144d83415e4891c7314cdadfaeb4
PAPIDAKI <papidakis@gmail.com>
2019-10-07 14:37:46 +0300

    create user model and migrate

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Returns the full page title
+  def full_title(page_title='')
+    base_title = 'Certifable'
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
