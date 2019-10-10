8445537604c78329538cf53ebd7b5b2ed4e7c033
PAPIDAKI <papidakis@gmail.com>
2019-10-10 14:17:32 +0300

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'static_pages/home'
-  get 'static_pages/contact'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get '/home'=>'static_pages#home'
+  get '/contact'=>'static_pages#contact'
+  get '/help'=>'static_pages#help'
+  get '/about' =>'static_pages#about'
   root 'static_pages#home'
+  
+  get '/signup' =>'users#new'
 end
