78be7de4cfe9551651c7ad5f09711b776e12e43c
PAPIDAKI <papidakis@gmail.com>
2019-10-24 12:51:30 +0300

    untested advanced login and js log out bug needs fixing

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,9 +18,27 @@ module SessionsHelper
     #if current_user && !current_user.nil
   end
 
-  #Log out the current user
-    def log_out
+  #Logs  out the current user
+  def log_out
+    forget(current_user) # for persistent session 
       session.delete(:user_id)
       @current_user=nil
-    end
+  end
+
+  #Remembers a user in a persisitent session
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] =user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+
+  # Forgets a user in a persistent session
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   attr_accessor :remember_token
-  before_save {email.downcase!}
+  before_save {self.email = email.downcase}
 
   validates :name,presence: true,length:{minimum:2, maximum: 50}
   VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -26,9 +26,21 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64 #SecureRandom is a library included in Rails 
   end
 
+  # Remembers a user in the database for use in persistent sessons 
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest ,User.digest(remember_token)) # we make the rember_tokem an accessor and assignit to the User.remember attribute by updating it
+  end
+
+  def forget
+    self.update_attribute(:remember_digest,nil)
+  end
+
+
+  #Returns true is the given token matches the digest 
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(rember_digest).is_password?(remember_token)
   end
 end
 
