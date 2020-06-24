c06981a59d9267970ef9f6cc05b83b0b815134c3
mpa <papidakis@gmail.com>
2020-06-24 17:17:43 +0300

    navbar and  jumbotron color change

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+100.times do |i|
+  User.create(
+    name:Faker::Name.name,
+    email: "a"+i.to_s+"@a.com",
+    password:'1234',
+    password_confirmation:'1234',
+    admin:false
+  )
+  print '.' if i%100==0
+end
+puts 
